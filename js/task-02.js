f92616a8fa00a182583748ecd8bb0c26d3a39e7a
unknown <Kassir@kassa2.1479.apteka>
2023-06-18 17:07:22 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  fragment.appendChild(listItem);
+});
+
+ingredientsList.appendChild(fragment);
+
+
